4b96e7ef9f1a4c2b36ee96caaf4e570ab20460d0
Guillaume Lastecoueres <px9e@gmx.fr>
2016-11-25 18:18:23 +0100

    MONGODB

--- Omnomnomnom.py
+++ Omnomnomnom.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import json
+import re
+from flask import Flask, request
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/search')
+def search():
+    search_term = request.args.get("searchparam")
+    regex = re.compile(".*{0}.*".format(search_term))
+    results = mongo.db["item"].find({"attributes.name": {"$regex": regex}})
+    results = [res["attributes"] for res in results]
+    return json.dumps({"results": results})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=80)
