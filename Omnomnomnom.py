55a7120c131ffaa40bf17d25bc02ec8a800d8645
Guillaume Lastecoueres <px9e@gmx.fr>
2016-11-26 15:01:58 +0100

    Limit parameter

--- Omnomnomnom.py
+++ Omnomnomnom.py
@@ -10,9 +10,13 @@ mongo = PyMongo(app)
 @app.route('/search')
 def search():
     search_term = request.args.get("searchparam")
+    limit = request.args.get("limit", 10)
+    limit = int(limit)
+    if limit > 1000:
+        limit = 1000
     regex = re.compile(".*{0}.*".format(search_term))
     results = mongo.db["item"].find({"attributes.name": {"$regex": regex}})
-    results = results[:10]
+    results = results[:limit]
     results = [res["attributes"] for res in results]
     return json.dumps({"results": results})
 
