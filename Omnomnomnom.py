85c89d0322f956e79525c611a813b56259a7c93d
Guillaume Lastecoueres <px9e@gmx.fr>
2016-11-26 22:46:41 +0100

    Introduce CO2 computation

--- Omnomnomnom.py
+++ Omnomnomnom.py
@@ -37,7 +37,7 @@ def search():
             except Exception:
                 g = Nominatim()
                 d1 = g.geocode(country)
-                d2 = g.geocode()
+                d2 = g.geocode(res["origins"][0])
                 co2 = compute_co2(vincenty(d1.point, d2.point).kilometers)
                 res["estimated_co2"] = co2
                 mongo.db["traject"].insert({"from": res["origins"][0], "to": country, "co2": co2})
