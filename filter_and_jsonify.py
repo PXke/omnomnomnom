a3df6e6bf3166ac503df4f9f468b6ce3c646fc13
Odissea <theo.dupuis@gmail.com>
2016-11-27 11:29:34 +0100

    Almost done withe the reshaping of the db

--- filter_and_jsonify.py
+++ filter_and_jsonify.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import sys
 import time
+import datetime
+import json
 
 def print_progress(iteration, total, prefix='', suffix='', decimals=1,
 	barLength=100):
@@ -23,10 +25,30 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1,
 	    sys.stdout.write('\n')
 	sys.stdout.flush()
 
+def get_nutrient_from_list(list_, name) :
+
+	print("##################################")
+	print("##################################")
+	print("Liste : ", type(list_), len(list_))
+	for dict_ in list_ :
+		print(dict_["name"])
+		print("Dico : ", type(dict_))
+		if dict_["name"] == name:
+			print("##################################")
+			print("##################################\n\n")
+			return json.dumps(dict_)
+	print("##################################")
+	print("##################################\n\n")
+	return None
+
+
 def clean_db(df) :
+
+	start = time.time()
 
 	for index, line in enumerate(df["results"][:]) :
 
+		print("###########",type(get_nutrient_from_list(line["nutrients"], "Energy")))
 		df['quantity'].iloc[index] = line['quantity']
 		df['ingredients'].iloc[index] = line['ingredients']
 		df['origins'].iloc[index] = line['origins']
@@ -46,10 +68,21 @@ def clean_db(df) :
 		df["origins_names_it"].iloc[index] = line["origin-translations"].get("it")
 		df["origins_names_de"].iloc[index] = line["origin-translations"].get("de")
 		df["ingredients_names_fr"].iloc[index] = line["ingredients-translations"].get("fr") 
-		df["ingredients_names_en"].iloc[index] = line["ingredients-translations"].get("en") 
+		df["ingredients_names_en"].iloc[index] = line["ingredients-translations"].get("en")
 		df["ingredients_names_it"].iloc[index] = line["ingredients-translations"].get("it") 
-		df["ingredients_names_de"].iloc[index] = line["ingredients-translations"].get("de") 
-		print_progress(index+1,df.shape[0], decimals=2)
+		df["ingredients_names_de"].iloc[index] = line["ingredients-translations"].get("de")
+		"""df['Energy'].iloc[index] = {"LOL !" : 2}#get_nutrient_from_list(line["nutrients"],'Energy')
+		df['Energy (kCal)'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Energy (kCal)')
+		df['Protein'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Protein')
+		df['Fat'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Fat')
+		df['Saturated fat'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Saturated fat')
+		df['Carbohydrates'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Carbohydrates')
+		df['Sugars'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Sugars')
+		df['Salt'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Salt')
+		df['Fiber'].iloc[index] = get_nutrient_from_list(line["nutrients"],'Fiber')"""
+
+		#print_progress(index+1,df.shape[0], decimals=2)
+		#print("{0}/{1} at {2}".format(index+1,df.shape[0],datetime.datetime.now()))
 	df = df.drop(["results"], axis=1)
 	return df
 
@@ -82,11 +115,19 @@ db["ingredients_names_fr"]= np.nan
 db["ingredients_names_en"]= np.nan
 db["ingredients_names_it"]= np.nan
 db["ingredients_names_de"]= np.nan
-print("...")
+db['Energy'] = np.nan
+db['Energy (kCal)'] = np.nan
+db['Protein'] = np.nan
+db['Fat'] = np.nan
+db['Saturated fat'] = np.nan
+db['Carbohydrates'] = np.nan
+db['Sugars'] = np.nan
+db['Salt'] = np.nan
+db['Fiber'] = np.nan
 
 
 new_db = clean_db(db)
-print(new_db)
 
 
-new_db.to_csv(OUTPUTPATH+"/modified_db.csv")
+new_db.to_csv(OUTPUTPATH+"/modified_db.csv", encoding="utf8")
+new_db.to_json(OUTPUTPATH+"/modified_db.json", force_ascii=False)
